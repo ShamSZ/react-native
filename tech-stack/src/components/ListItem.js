cd1c2d9da254e185fee03cd87880a80f352ce6f0
ShamSZ <sham.szoda@gmail.com>
2019-01-27 13:35:21 +0000

    Added logic to display library description on press.

--- tech-stack/src/components/ListItem.js
+++ tech-stack/src/components/ListItem.js
@@ -5,9 +5,22 @@ import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
+  renderDescription() {
+    const { library, expanded } = this.props;
+
+    if (expanded) {
+      return (
+        <Text>
+          {library.item.description}
+        </Text>
+      );
+    }
+  }
+
   render() {
     const { titleStyle } = styles;
     const { id, title } = this.props.library.item;
+
 
     return (
       <TouchableWithoutFeedback
@@ -19,6 +32,7 @@ class ListItem extends Component {
               {title}
             </Text>
           </CardSection>
+          {this.renderDescription()}
         </View>
       </TouchableWithoutFeedback>
     );
@@ -32,4 +46,11 @@ const styles = {
   }
 };
 
-export default connect(null, actions)(ListItem);
+function mapStateToProps(state, ownProps) {
+  // return true/false
+  const expanded = state.selectedLibraryId === ownProps.library.item.id;
+
+  return { expanded };
+}
+
+export default connect(mapStateToProps, actions)(ListItem);
