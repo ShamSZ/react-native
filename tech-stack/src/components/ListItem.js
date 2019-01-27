3db1266488b069e367ba68d5b7edab60156dad6b
ShamSZ <sham.szoda@gmail.com>
2019-01-27 13:44:37 +0000

    Nested description inside CardSection component and added layout animation.

--- tech-stack/src/components/ListItem.js
+++ tech-stack/src/components/ListItem.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
-import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import {
+  Text,
+  TouchableWithoutFeedback,
+  View,
+  LayoutAnimation
+} from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
+  componentDidUpdate() {
+    LayoutAnimation.spring();
+  }
+
   renderDescription() {
     const { library, expanded } = this.props;
 
     if (expanded) {
       return (
-        <Text>
-          {library.item.description}
-        </Text>
+        <CardSection>
+          <Text>
+            {library.item.description}
+          </Text>
+        </CardSection>
       );
     }
   }
