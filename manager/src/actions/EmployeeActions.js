c2d3cca371c6c496b2ec3ec3488ec7d2c94fb55a
ShamSZ <sham.szoda@gmail.com>
2019-02-06 23:35:45 +0000

    Added functionality to save new employee to firebase.

--- manager/src/actions/EmployeeActions.js
+++ manager/src/actions/EmployeeActions.js
@@ -1,3 +1,4 @@
+import firebase from 'firebase';
 import {
   EMPLOYEE_UPDATE
 } from './types';
@@ -11,5 +12,7 @@ export function employeeUpdate({ prop, value }) {
 }
 
 export function employeeCreate({ name, phone, shift }) {
-  console.log(name, phone, shift);
+  const { currentUser } = firebase.auth();
+  firebase.database().ref(`/users/${currentUser.uid}/employees`)
+    .push({ name, phone, shift });
 }
